2f46b46ae7fd55fc432f07801d930025e2af3046
Phil Chen <06fahchen@gmail.com>
2023-12-07 21:21:07 +0800

    feat: day1 part1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,39 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n")
+    |> Stream.map(&find_digits/1)
+    |> Enum.sum()
+  end
+
+  defp find_digits(binary, acc \\ [])
+
+  for i <- 0..9 do
+    <<codepoint>> = Integer.to_string(i)
+
+    defp find_digits(<<unquote(codepoint), rest::binary>>, []) do
+      find_digits(rest, [unquote(i)])
+    end
+
+    defp find_digits(<<unquote(codepoint), rest::binary>>, [first | _rest]) do
+      find_digits(rest, [first, unquote(i)])
+    end
+  end
+
+  defp find_digits(<<_head, rest::binary>>, acc) do
+    find_digits(rest, acc)
+  end
+
+  defp find_digits(<<>>, []) do
+    0
+  end
+
+  defp find_digits(<<>>, [digit]) do
+    Integer.undigits([digit, digit])
+  end
+
+  defp find_digits(<<>>, [first, last]) do
+    Integer.undigits([first, last])
   end
 
   def part2(_args) do
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,9 +3,8 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(1, 2023)
     result = part1(input)
 
     assert result
